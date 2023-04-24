2bd630f9dbe83c23229ef6d22f4de05af45e7db8
Alikri <alikri@gmail.com>
2023-04-24 16:14:04 +0200

    refactor(extended-1.2): add alt to img in js, so newly generated delete icons have them as well

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className = "button-delete";
     deleteButton.classList.add("button");
     deleteButtonImg.src = './remove.svg';
+		deleteButtonImg.alt = "";
     deleteButtonImg.classList.add("button-delete__img");
     deleteButton.appendChild(deleteButtonImg);
 
